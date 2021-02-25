97bcae8f70cb8bc0ad2e3f78d9abc5b0a8a9a408
marina-grechukha <mhrechuk@cisco.com>
2021-02-26 00:57:25 +0200

    Add Sign In form layout

--- src/views/sign-in.vue
+++ src/views/sign-in.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    Sign In
-  </div>
+  <form class="w-72 mx-auto mt-20">
+    <div class="mb-4">
+      <input
+        class="w-full border border-gray-300 rounded-md px-4 py-2"
+        type="text"
+        name="email"
+        placeholder="Enter Email"
+      />
+    </div>
+
+    <div class="mb-4">
+      <input
+        class="w-full border border-gray-300 rounded-md px-4 py-2"
+        type="password"
+        name="password"
+        placeholder="Enter Password"
+      />
+    </div>
+
+    <button class="w-full rounded-md px-4 py-2 bg-green-500 text-white" type="submit">Sign In</button>
+  </form>
 </template>
